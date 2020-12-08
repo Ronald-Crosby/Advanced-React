1d8612b9f90a46910ca00b6e9dcbd4e242789ac9
Olly Robinson <49555108+Ronald-Crosby@users.noreply.github.com>
2020-12-08 15:40:58 +0000

    lesson 15

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if theyre logged in
+
+    // this is how we access the db that we added to context earlier in the createServer file
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
+  },
+};
 
 module.exports = Mutation;
--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -5,7 +5,7 @@ import { endpoint } from '../config';
 function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: operation => {
+    request: (operation) => {
       operation.setContext({
         fetchOptions: {
           credentials: 'include',
@@ -13,6 +13,7 @@ function createClient({ headers }) {
         headers,
       });
     },
+    connectToDevTools: true,
   });
 }
 
