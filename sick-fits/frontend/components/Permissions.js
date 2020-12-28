f5c6b1bf10ccf63637b5c56ec57cf46a1f34127e
Olly Robinson <49555108+Ronald-Crosby@users.noreply.github.com>
2020-12-28 14:45:11 +0000

    lesson 35

--- sick-fits/frontend/components/Permissions.js
+++ sick-fits/frontend/components/Permissions.js
@@ -1,6 +1,10 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/static-property-placement */
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import React from 'react';
+import PropTypes from 'prop-types';
 import Error from './ErrorMessage';
 import Table from './styles/Table';
 import SickButton from './styles/SickButton';
@@ -40,14 +44,14 @@ const Permissions = (props) => (
                 <th>Name</th>
                 <th>Email</th>
                 {possiblePermissions.map((permission) => (
-                  <th>{permission}</th>
+                  <th key={permission}>{permission}</th>
                 ))}
                 <th>🤌</th>
               </tr>
             </thead>
             <tbody>
               {data.users.map((user) => (
-                <User user={user} />
+                <UserPermissions key={user.id} user={user} />
               ))}
             </tbody>
           </Table>
@@ -57,7 +61,39 @@ const Permissions = (props) => (
   </Query>
 );
 
-class User extends React.Component {
+class UserPermissions extends React.Component {
+  // this is how to set prop types on all the keys of an object
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array,
+    }).isRequired,
+  };
+
+  state = {
+    permissions: this.props.user.permissions,
+  };
+
+  handlePermissionChange = (e) => {
+    const checkbox = e.target;
+    // get a copy of the current permissions
+    let updatedPermissions = [...this.state.permissions];
+    console.log(updatedPermissions);
+    // remember this all happens when we click on a checkbox!
+    // if the box you just clicked is now checked (ie if it was unchecked before), add the value of of that checkbox (eg ADMIN) to the array of permissions. this array is just a copy of the real one, which we will overwrite later
+    if (checkbox.checked) {
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(
+        (permission) => permission !== checkbox.value
+      );
+    }
+    console.log(updatedPermissions);
+    this.setState({ permissions: updatedPermissions });
+  };
+
   render() {
     const { user } = this.props;
     return (
@@ -67,7 +103,13 @@ class User extends React.Component {
         {possiblePermissions.map((permission) => (
           <td>
             <label htmlFor={`${user.id}-permission-${permission}`}>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                // this sets the state to checked if the current permission on the iterator is included in the list of permissions for the user in question
+                checked={this.state.permissions.includes(permission)}
+                value={permission}
+                onChange={this.handlePermissionChange}
+              />
             </label>
           </td>
         ))}
